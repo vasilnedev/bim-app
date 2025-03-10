b442c5de39f9338a9648946016268aee2844008c
vasilnedev <vasilnedev55@gmail.com>
2025-03-10 12:27:31 +0000

    Commit all changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker exec dev-stack-bim-app-1 npm run build
-docker exec dev-stack-bim-app-1 npm run deploy
+docker exec bim-app-bim-app-1 npm run build
+docker exec bim-app-bim-app-1 npm run deploy
