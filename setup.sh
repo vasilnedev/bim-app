38ff187486ed8df1154e3733a18f5a6f93ec84a1
vasilnedev <vasilnedev55@gmail.com>
2025-03-11 19:35:25 +0000

    none

--- setup.sh
+++ setup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-echo "Grant full access to node-red folder..."
+echo "A. Grant full access to node-red folder..."
 chmod 777 node-red
 
-echo "Pull Docker images..."
+echo "B. Pull Docker images..."
 docker image pull neo4j
 docker image pull minio/minio
 docker image pull node
 docker image pull nodered/node-red
 docker image pull eclipse-mosquitto
 
-echo "Install node modules..."
+echo "C. Install node modules..."
 docker run -v $(pwd)/bim-app:/usr/src/app -w /usr/src/app node npm install
 docker run -v $(pwd)/bim-proxy:/usr/src/app -w /usr/src/app node npm install
 docker run -v $(pwd)/node-red:/data -w /data node npm install
 
-echo "Starting Docker containers..."
+echo "D. Starting Docker containers..."
 docker compose up -d
 
-echo "Create manually the MinIO buckets:"
+echo "E. Create manually the MinIO buckets:"
 echo "1. Open http://localhost:9000"
 echo "2. Create buckets named 'bim-app' and 'models'"
 echo "3. Upload the samples from '/sample-models' to 'models' bucket"
 echo "4. Grant Public Access to both 'bim-app' and 'models' buckets"
 read -p "When ready select Y to continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-echo "Building the BIM App..."
+echo "F. Building the BIM App..."
 docker exec bim-app-bim-app-1 npm run build
 docker exec bim-app-bim-app-1 npm run deploy
 
@@ -35,5 +35,5 @@ echo "Open http://localhost:9000/bim-app/index.html to access the deploied BIM A
 echo "Open http://localhost:3000/bim-app/index.html to access the deploied BIM App behind the proxy"
 echo "Open http://localhost:1880 to access Node-RED"
 echo "Open http://localhost:9000 to access MinIO"
-echo "Open http://localhost:1883 to access MQTT broker"
+echo "Use http://localhost:1883 entry to MQTT broker"
 echo "Open http://localhost:7474 to access Neo4j"
