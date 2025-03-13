3d25c3ce59fc849d368ed1fd9b1206cdb9375749
vasilnedev <vasilnedev55@gmail.com>
2025-03-13 20:01:08 +0000

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ docker image pull minio/minio
 docker image pull node
 docker image pull nodered/node-red
 docker image pull eclipse-mosquitto
+docker image pull apache/tika
 
 echo "C. Install node modules..."
 docker run -v $(pwd)/bim-app:/usr/src/app -w /usr/src/app node npm install
@@ -18,20 +19,19 @@ docker run -v $(pwd)/node-red:/data -w /data node npm install
 echo "D. Starting Docker containers..."
 docker compose up -d
 
-echo "E. Create MinIO buckets:"
-docker exec bim-app-minio-1 mc mb bim-app
-docker exec bim-app-minio-1 mc mb models
-docker exec bim-app-minio-1 mc mb documents
+echo "E. Create MinIO buckets, copy sample models and build the app ..."
+docker exec -w /data bim-app-minio-1 mc mb bim-app
+docker exec -w /data bim-app-minio-1 mc mb models
+docker exec -w /data bim-app-minio-1 mc mb documents
+docker exec -v $(pwd)/sample-models:/home bim-app-minio-1 mc cp --recursive /home /data/models
+docker exec bim-app-bim-app-1 npm run build
+docker exec bim-app-bim-app-1 npm run deploy
 
 echo "F. Grant public access to all folders and upload sample model files:"
 echo "1. Open http://localhost:9000"
-echo "3. Upload the samples from '/sample-models' to 'models' bucket"
-echo "4. Grant Public Access to all buckets buckets"
+echo "2. Login with user and password: myminioadmin"
+echo "3. Grant Public Access to all buckets buckets"
 read -p "When ready select Y to continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-
-echo "G. Building the BIM App..."
-docker exec bim-app-bim-app-1 npm run build
-docker exec bim-app-bim-app-1 npm run deploy
 
 echo "Setup complete!"
 echo "Open http://localhost:5173 to access the development BIM App"
