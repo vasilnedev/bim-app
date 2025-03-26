72b992ab116bc57a20eeea47da709a5bb14dbcb3
vasilnedev <vasilnedev55@gmail.com>
2025-03-26 10:18:00 +0000

    Deleted node-red and bim-proxy; added bim-server

--- setup.sh
+++ setup.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-echo "A. Grant full access to node-red folder to allow access for Node-Red server..."
-chmod 777 node-red
-
-echo "B. Pull Docker images..."
+echo "A. Pull Docker images..."
 docker image pull neo4j
 docker image pull minio/minio
 docker image pull node
-docker image pull nodered/node-red
-docker image pull eclipse-mosquitto
 docker image pull apache/tika
 
-echo "C. Install node modules..."
+echo "B. Install node modules..."
 docker run -v $(pwd)/bim-app:/usr/src/app -w /usr/src/app node npm install
-docker run -v $(pwd)/bim-proxy:/usr/src/app -w /usr/src/app node npm install
-docker run -v $(pwd)/node-red:/data -w /data node npm install
+docker run -v $(pwd)/bim-server:/usr/src/app -w /usr/src/app node npm install
 
-echo "D. Starting Docker containers..."
+echo "C. Starting Docker containers..."
 docker compose up -d
 
-echo "E. Create MinIO buckets, copy sample models and build the app ..."
+echo "D. Create MinIO buckets, copy sample models and build the app ..."
 docker exec -w /data bim-app-minio-1 mc mb bim-app
 docker exec -w /data bim-app-minio-1 mc mb models
 docker exec -w /data bim-app-minio-1 mc mb documents
@@ -27,7 +21,7 @@ docker exec bim-app-bim-app-1 npm run sample-models
 docker exec bim-app-bim-app-1 npm run build
 docker exec bim-app-bim-app-1 npm run deploy
 
-echo "F. Grant public access to all folders and upload sample model files:"
+echo "E. Grant public access to all folders and upload sample model files:"
 echo "1. Open http://localhost:9000"
 echo "2. Login with user and password: myminioadmin"
 echo "3. Grant Public Access to all buckets buckets"
@@ -36,8 +30,5 @@ read -p "When ready select Y to continue? (Y/N): " confirm && [[ $confirm == [yY
 echo "Setup complete!"
 echo "Open http://localhost:5173 to access the development BIM App"
 echo "Open http://localhost:9000/bim-app/index.html to access the deploied BIM App"
-echo "Open http://localhost:3000/bim-app/index.html to access the deploied BIM App behind the proxy"
-echo "Open http://localhost:1880 to access Node-RED"
-echo "Open http://localhost:9000 to access MinIO"
-echo "Use http://localhost:1883 entry to MQTT broker"
+echo "Open http://localhost:9000 to access MinIO admin dashboard"
 echo "Open http://localhost:7474 to access Neo4j"
