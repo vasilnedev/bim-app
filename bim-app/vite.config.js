7a95637c577589bfe0e36c9797dfcd28a9ad57d7
vasilnedev <vasilnedev55@gmail.com>
2025-03-26 12:30:07 +0000

    Add alloedHosts to vite.config.js

--- bim-app/vite.config.js
+++ bim-app/vite.config.js
@@ -5,8 +5,10 @@ export default {
     },
     // set a proxy to minio server for the dev server
     server: {
+        allowedHosts: ['localhost','bim-app.laptop','bim-app.server'],
         proxy: {
           '/models': 'http://minio:9000'
         }
     }
+    
 };
